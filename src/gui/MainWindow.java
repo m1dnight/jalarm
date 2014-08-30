baabd5a6dbb025af230ad4f84d098e56f16610ca
Christophe De Troyer <christophe.detroyer@gmail.com>
2014-08-30 17:51:16 +0200

    Minor changes. Created jar file that works. Addec icon.

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -23,13 +23,13 @@ public class MainWindow
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AlarmFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AlarmFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AlarmFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AlarmFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -37,7 +37,7 @@ public class MainWindow
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
 			public void run() {
-                new NewJFrame().setVisible(true);
+                new AlarmFrame().setVisible(true);
             }
         });
 	}
